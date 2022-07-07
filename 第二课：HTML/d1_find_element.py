f003b245029ef11c5dbbafe43514add003aa5db0
Administrator <wfd19940918@163.com>
2022-07-07 22:35:48 +0800

    代码更新

--- 第二课：HTML/d1_find_element.py
+++ 第二课：HTML/d1_find_element.py
@@ -16,16 +16,34 @@ driver.get('http://www.douban.com')
 # elem 是一个叫做 WebElement 的对象
 elem = driver.find_element_by_name('q')
 print(elem)
-
 # 获取标签名
 print(elem.tag_name)
-
 # 属性、方法
 # 输入
-print(elem.send_keys('王者荣耀'))
-
+elem.send_keys('王者荣耀')
+# 清空输入框的内容
+elem.clear()
 # 获取到该元素的上一级
 print(elem.parent)
+# 获取元素的属性
+print(elem.get_attribute('maxlength'))
 
-driver.quit()
 # 第二道面试题：find_element和find_elements的区别
+# find_element 得到的是一个 WebElement 的对象，只会返回查到的第一个元素，找不到时会报错:NoSuchElementException
+# find_elements 得到的是一个列表，返回所有查询到的元素，找不到时返回一个空列表
+elems = driver.find_elements_by_name('q')
+print(elem)
+print(elems)
+
+# 判断元素是否存在
+if driver.find_elements_by_name('w'):
+    print("元素存在")
+else:
+    print("元素不存在")
+
+try:
+    driver.find_element_by_name('w')
+    print("元素存在")
+except:
+    print("元素不存在")
+
---
+++ 第二课：HTML/d2_html.py
@@ -0,0 +1,9 @@
+"""
+# @Time : 2022/7/7 0007   21:01
+# @File : d2_html
+# @Project : web_testing
+"""
+
+"""
+HTML
+"""
